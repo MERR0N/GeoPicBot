51ad0961efd443938e9c3f90e9ab4149faa6274a
Maxim MERRON <m3rr0n@gmail.com>
2017-09-06 13:38:55 +0200

    Now it works. Probably

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def geo(longitude, latitude, radius=DEFAULT_RADIUS):
         username = ig['user']['username']
         link = ig['link']
         location = ig['location']['name'] if ig['location']['name'] else 'Unknown place'
-        date = datetime.fromtimestamp(ig['created_time'])
+        date = datetime.fromtimestamp(int(ig['created_time']))
         description = ig['caption']['text'] if ig['caption'] else 'No description'
 
         instagram_response = "Username: {}\nLink: {}\nLocation: {}\nDate: {}\nDescription: {}\n\n\n" \
@@ -36,7 +36,7 @@ def geo(longitude, latitude, radius=DEFAULT_RADIUS):
         id = vkcom['owner_id']
         link = 'https://vk.com/photo{}_{}'.format(vkcom['owner_id'], vkcom['pid'])
         img = vkcom['src_big']
-        date = datetime.fromtimestamp(vkcom['created'])
+        date = datetime.fromtimestamp(int(vkcom['created']))
         description = vkcom['text'] if vkcom['text'] else 'No description'
 
         vk_response = "ID: id{}\nLink: {}\nImage: {}\nDate: {}\nDescription: {}\n\n\n" \
@@ -54,9 +54,17 @@ def start(message):
 @bot.message_handler(commands=['location'])
 def start(message):
     args = message.text.split()[1:]
-    geo_result = geo(*args)
-    for response in geo_result:
-        bot.send_message(message.chat.id, response)
+    if len(args) < 2:
+        bot.send_message(message.chat.id, 'You must send longitude and latitude. And maybe radius')
+    else:
+        try:
+            safe_args = [float(arg) for arg in args]
+        except ValueError:
+            bot.send_message(message.chat.id, 'Unfortunately, I can not understand these coordinates')
+        else:
+            geo_result = geo(*safe_args)
+            for response in geo_result:
+                bot.send_message(message.chat.id, response)
 
 
 @bot.message_handler(content_types=['location'])
